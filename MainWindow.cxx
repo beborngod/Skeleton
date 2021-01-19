18ad9f5f342155a9aeeeca3214c4df18ab7c8575
beborngod <oleksiibelov@gmail.com>
2021-01-19 20:31:41 +0200

    added splitting to 2 windows in code

--- MainWindow.cxx
+++ MainWindow.cxx
@@ -1,13 +1,23 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
-static int zoom = 0;
+#include <iostream>
+
+static int zoom_first_window = 0;
+static int zoom_second_window = 0;
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    setCentralWidget(ui->edit);
+    splitter = new QSplitter(Qt::Horizontal,this);
+
+    edit = new QTextEdit(this);
+
+    splitter->addWidget(edit);
+
+    setCentralWidget(splitter);
+
 
     QPixmap newPix("file.svg");
     QPixmap openPix("045-file.svg");
@@ -15,12 +25,14 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
     QPixmap minusPix("zoom_out.svg");
     QPixmap plusPix("zoom_in.svg");
     QPixmap clearPix("017-trash.svg");
+    QPixmap splitPix("013-layers.svg");
 
     auto newFile = ui->toolbar->addAction(QIcon(newPix),"New file");
     auto openFile = ui->toolbar->addAction(QIcon(openPix),"Open file");
     auto saveFile = ui->toolbar->addAction(QIcon(savePix),"Save file");
     auto zoomIn = ui->toolbar->addAction(QIcon(plusPix),"Zoom in");
     auto zoomOut = ui->toolbar->addAction(QIcon(minusPix),"Zoom out");
+    auto split = ui->toolbar->addAction(QIcon(splitPix),"Split display");
 
     ui->toolbar->addSeparator();
 
@@ -34,6 +46,8 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
     connect(zoomIn,&QAction::triggered, this, &MainWindow::zoomTextIn);
     connect(zoomOut,&QAction::triggered, this, &MainWindow::zoomTextOut);
 
+    connect(split,&QAction::triggered, this, &MainWindow::splitDisplay);
+
 }
 
 MainWindow::~MainWindow()
@@ -43,7 +57,9 @@ MainWindow::~MainWindow()
 
 void MainWindow::newFile()
 {
-    ui->edit->clear();
+    focus = QApplication::focusWidget();
+
+    edit->clear();
 
     QMessageBox::StandardButton reply = QMessageBox::question(
     this,"Save the file?","Do you want to save the file?",QMessageBox::Yes | QMessageBox::No);
@@ -55,7 +71,10 @@ void MainWindow::newFile()
         if(not s_text.isEmpty()){
             QFile file(s_text);
             file.open(QIODevice::WriteOnly);
-            file.write(ui->edit->toPlainText().toUtf8());
+            if(edit == focus)
+                file.write(edit->toPlainText().toUtf8());
+            else if(second_edit == focus)
+                file.write(second_edit->toPlainText().toUtf8());
             file.close();
         }
     }
@@ -63,6 +82,8 @@ void MainWindow::newFile()
 
 void MainWindow::openFile()
 {
+    focus = QApplication::focusWidget();
+
     QString c_text = QFileDialog::getOpenFileName();
     QString s_text = c_text.simplified();
 
@@ -70,8 +91,16 @@ void MainWindow::openFile()
         QFile file(s_text);
         if (file.open(QIODevice::ReadOnly))
          {
-            ui->edit->clear();
-            ui->edit->append(QString(file.readAll()));
+            if(focus == edit){
+                std::cout<<"edit->isActiveWindow()"<<'\n';
+                edit->clear();
+                edit->append(QString(file.readAll()));
+            }
+            else if(focus == second_edit){
+                std::cout<<"second_edit->isActiveWindow()"<<'\n';
+                second_edit->clear();
+                second_edit->append(QString(file.readAll()));
+            }
         }
         file.close();
     }
@@ -79,37 +108,74 @@ void MainWindow::openFile()
 
 void MainWindow::saveFile()
 {
+    focus = QApplication::focusWidget();
+
     QString c_text = QFileDialog::getSaveFileName();
     QString s_text = c_text.simplified();
 
     if(not s_text.isEmpty()){
         QFile file(s_text);
         file.open(QIODevice::WriteOnly);
-        file.write(ui->edit->toPlainText().toUtf8());
+        if(focus == edit)
+            file.write(edit->toPlainText().toUtf8());
+        else if(focus == second_edit)
+            file.write(second_edit->toPlainText().toUtf8());
         file.close();
     }
 }
 
 void MainWindow::zoomTextIn()
 {
-    ui->edit->zoomIn();
-    ++zoom;
+    focus = QApplication::focusWidget();
+
+    if(focus == edit){
+        edit->zoomIn();
+        ++zoom_first_window;
+    }else if(focus == second_edit){
+        second_edit->zoomIn();
+        ++zoom_second_window;
+    }
 }
 
 void MainWindow::zoomTextOut()
 {
-    if(zoom >= 0){
-        ui->edit->zoomOut();
-        --zoom;
+    focus = QApplication::focusWidget();
+
+    if(focus == edit){
+        if(zoom_first_window >= 0){
+            edit->zoomOut();
+            --zoom_first_window;
+        }
+    }else if(focus == second_edit){
+            if(zoom_second_window >= 0){
+                second_edit->zoomOut();
+                --zoom_second_window;
+            }
     }
+}
+
+void MainWindow::splitDisplay()
+{
+    if(splitter->count() == 1){
+        second_edit = new QTextEdit();
+        splitter->addWidget(second_edit);
+    }
+    else
+        second_edit->deleteLater();
+
 }
 
 void MainWindow::clear()
 {
+    focus = QApplication::focusWidget();
+
     QMessageBox::StandardButton reply = QMessageBox::question(
     this,"Clear the file?","Do you want to clear the file?",QMessageBox::Yes | QMessageBox::No);
 
     if (reply == QMessageBox::Yes){
-        ui->edit->clear();
+        if(focus == edit)
+            edit->clear();
+        else if(focus == second_edit)
+            second_edit->clear();
     }
 }
--- MainWindow.h
+++ MainWindow.h
@@ -10,6 +10,9 @@
 #include <QStatusBar>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QSplitter>
+#include <QHBoxLayout>
+#include <QTextEdit>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,14 +22,19 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
     Ui::MainWindow *ui;
+    QSplitter *splitter;
+    QTextEdit *edit;
+    QTextEdit *second_edit;
+    QWidget *focus;
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-private slots:
+protected slots:
     void newFile();
     void openFile();
     void saveFile();
     void zoomTextIn();
     void zoomTextOut();
+    void splitDisplay();
     void clear();
 };
